a1260b0c57e6c88ea16734e61752ec433ff8edea
jimmymetts <jimmymetts@bellsouth.net>
2020-02-03 14:56:23 -0600

    added onChange to input

--- src/components/HookCounterThree.js
+++ src/components/HookCounterThree.js
@@ -5,9 +5,21 @@ function HookCounterThree() {
     const [name, setName] = useState({firstName:'', lastName: ''})
 
     return (
-        <div>
-            
-        </div>
+
+    <form>
+        <input input='text' 
+        value={name.firstName} 
+        onChange={e => setName({firstName: e.target.value})} />
+
+        <input input='text' 
+        value={name.lastName} 
+        onChange={e => setName({lastName: e.target.value})} />
+        
+
+    <h2>You first name is - {name.firstName}</h2>
+    <h2>You first last is - {name.lastName}</h2>
+
+    </form>
     )
 }
 
