9b7b83ce5703d32e9fb0008c1a52ac00268dfe24
jimmymetts <jimmymetts@bellsouth.net>
2020-02-03 15:12:27 -0600

    add hookcounterfour

--- src/components/HookCounterThree.js
+++ src/components/HookCounterThree.js
@@ -21,6 +21,7 @@ function HookCounterThree() {
 
     <h2>You first name is - {name.firstName}</h2>
     <h2>You first last is - {name.lastName}</h2>
+    <h2>{JSON.stringify(name)}</h2>
 
     </form>
     )
