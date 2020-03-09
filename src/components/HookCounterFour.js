220124f1b52a5490651ccbf5bf9aa55bc54f36f3
jimmymetts <jimmymetts@bellsouth.net>
2020-03-08 19:37:44 -0500

    added button onClick

--- src/components/HookCounterFour.js
+++ src/components/HookCounterFour.js
@@ -9,14 +9,14 @@ function HookCounterFour() {
     const addItem = () => {
         setItems([...items, { 
             id: items.length, 
-            value: Math.floor(Math.random() * 10) + 1
+            value: Math.floor(Math.random() * 2) + 1
         }])
     }
 
 
     return (
         <div>
-            <button onClick={addItem}>Subtract</button>
+            <button onClick={addItem}>Add a number</button>
             <ul>
                 {items.map(item => (
                     <li key={item.id}> {item.value}</li>
