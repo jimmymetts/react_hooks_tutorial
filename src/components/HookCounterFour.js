d54513d610722f0db9e45571ca05dd8ca094442e
jimmymetts <jimmymetts@bellsouth.net>
2020-03-15 15:23:05 -0500

    renamed button

--- src/components/HookCounterFour.js
+++ src/components/HookCounterFour.js
@@ -16,7 +16,7 @@ function HookCounterFour() {
 
     return (
         <div>
-            <button onClick={addItem}>Add a</button>
+            <button onClick={addItem}>Add a Number</button>
             <ul>
                 {items.map(item => (
                     <li key={item.id}> {item.value}</li>
