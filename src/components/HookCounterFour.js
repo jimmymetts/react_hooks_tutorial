9f19447da6cead921fa4bde7cf0ebaf4de11516c
jimmymetts <jimmymetts@bellsouth.net>
2020-03-15 15:22:35 -0500

    changed hookCounterFour

--- src/components/HookCounterFour.js
+++ src/components/HookCounterFour.js
@@ -16,7 +16,7 @@ function HookCounterFour() {
 
     return (
         <div>
-            <button onClick={addItem}>Add </button>
+            <button onClick={addItem}>Add a</button>
             <ul>
                 {items.map(item => (
                     <li key={item.id}> {item.value}</li>
