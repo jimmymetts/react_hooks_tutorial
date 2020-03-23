e5d670233af061aa6944b8f8441aa565237db993
jimmymetts <jimmymetts@bellsouth.net>
2020-03-23 18:54:49 -0500

    started ClassCounterOne Component

---
+++ src/components/ClassCounterOne.js
@@ -0,0 +1,10 @@
+import  React, { Component } from 'react'
+
+class ClassCounterOne extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0
+        }
+    }
+}
