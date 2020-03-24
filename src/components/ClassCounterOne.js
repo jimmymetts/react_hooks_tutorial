70c5d095fe672c25563e858f8ca88e4c8e53cc12
jimmymetts <jimmymetts@bellsouth.net>
2020-03-23 19:02:00 -0500

    added button with onClick

--- src/components/ClassCounterOne.js
+++ src/components/ClassCounterOne.js
@@ -15,4 +15,16 @@ class ClassCounterOne extends Component {
     componentDidUpdate(prevProps, prevState) {
         document.title = `Clicked ${this.state.count} times`
     }
+
+    render() {
+        return (
+            <div>
+                <button onClick={() => this.setState({ count: this.state.count + 1})}>
+                    Click {this.state.count} times
+                </button>
+            </div>
+        )
+    }
 }
+
+export default ClassCounterOne
